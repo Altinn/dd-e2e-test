3abc51c90686066a3b1869d3bf8164b6945285ab
Lars Olav Kjølstad <lars.olav.kjolstad@digdir.no>
2025-07-09 11:36:57 +0200

    Working docker-compose with navigation between services

--- infra/pull-services.ps1
+++ infra/pull-services.ps1
@@ -3,3 +3,7 @@ Remove-Item -Path "./services" -Recurse -Force
 git clone https://github.com/Altinn/app-localtest.git ./services/app-localtest
 git clone https://altinn.studio/repos/digdir/oed.git ./services/oed
 git clone https://altinn.studio/repos/digdir/oed-declaration.git ./services/oed-declaration
+
+Copy-Item ./app-localtest/src/Services/LocalApp/Implementation/LocalAppHttp.cs ./services/app-localtest/src/Services/LocalApp/Implementation/LocalAppHttp.cs
+
+(Get-Content ./services/oed/App/HttpClients/HttpClientsRegistration.cs).Replace('https://{oedOptions.Host}', 'http://{oedOptions.Host}') | Set-Content ./services/oed/App/HttpClients/HttpClientsRegistration.cs
